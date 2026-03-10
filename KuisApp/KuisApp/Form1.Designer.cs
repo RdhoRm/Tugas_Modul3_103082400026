a063f68c928da72ce8fbb9d4d69f8e118511c521
Ridho Ramadhan <ridho.ramadan1016@gmail.com>
2026-03-10 22:12:36 +0700

    menambahkan implementasi kuis

--- KuisApp/KuisApp/Form1.Designer.cs
+++ KuisApp/KuisApp/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(284, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Apa fungsi utama dari GUI Builder?";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(284, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 70);
+            button1.TabIndex = 1;
+            button1.Text = "A";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(400, 130);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 70);
+            button2.TabIndex = 2;
+            button2.Text = "B";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(284, 222);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 62);
+            button3.TabIndex = 3;
+            button3.Text = "C";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(400, 222);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 62);
+            button4.TabIndex = 4;
+            button4.Text = "D";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(334, 315);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Status Jawaban";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
     }
 }
--- KuisApp/KuisApp/Form1.cs
+++ KuisApp/KuisApp/Form1.cs
@@ -6,5 +6,40 @@ namespace KuisApp
         {
             InitializeComponent();
         }
+        private void CekJawaban(string pilihanUser)
+        {
+            string kunciJawaban = "A";
+
+            if (pilihanUser == kunciJawaban)
+            {
+                label2.Text = "Jawaban benar kamu pintar";
+            }
+            else
+            {
+                label2.Text = "Jawaban salah kurang beruntung";
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CekJawaban("A");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CekJawaban("B");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CekJawaban("C");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CekJawaban("D");
+        }
     }
 }
